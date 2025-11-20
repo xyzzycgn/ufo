db898970d15cb4a362855d2e83e7d5965c2b980f
xyzzycgn <120264047+xyzzycgn@users.noreply.github.com>
2025-11-20 12:22:40 +0100

    POC dynamic creation of adapters for all electric-pole - missing key in locale produces "unknown key"-error for description(?) in tech gui, although setting localised_name in recipe fixed another

--- data-updates.lua
+++ data-updates.lua
@@ -2,6 +2,7 @@
 --- Created by xyzzycgn.
 ---
 local Log = require("__log4factorio__.Log")
+local data_util = require('__flib__.data-util')
 Log.setSeverity(Log.CONFIG)
 
 local fulg_attr = data.raw["lightning-attractor"]["fulgoran-ruin-attractor"]
@@ -15,5 +16,101 @@ fulg_attr.energy_source = {
   }
 fulg_attr.efficiency = 0.45
 
--- der fulgoriansichw blitzableiter
+-- der fulgorianische blitzableiter
 Log.logBlock(data.raw["lightning-attractor"]["fulgoran-ruin-attractor"], function(m)log(m)end)
+
+-- adapter item
+local ufo_adapter_item = data_util.copy_prototype(data.raw["item"]["small-lamp"], "ufo-adapter")
+local order = ufo_adapter_item.order or "ufo"
+ufo_adapter_item.icon = "__space-age__/graphics/icons/fulgoran-ruin-attractor.png"
+ufo_adapter_item.order = order .. "-a"
+
+-- adapter entity
+local ufo_adapter_entity = data_util.copy_prototype(data.raw["lamp"]["small-lamp"], "ufo-adapter")
+
+local ufo_adapter_recipe = data_util.copy_prototype(data.raw["recipe"]["small-lamp"], "ufo-adapter")
+ufo_adapter_recipe.ingredients = {
+    { type = "item", name = "small-lamp", amount = 1 },
+    { type = "item", name = "electronic-circuit", amount = 5 },
+    { type = "item", name = "advanced-circuit", amount = 2 },
+    { type = "item", name = "processing-unit", amount = 1 },
+}
+
+-- effects for tech
+local effects = {
+    { type = 'unlock-recipe', recipe = 'ufo-adapter' },
+}
+
+-- all recipes unlocked by tech
+local recipes = {
+    [1] = ufo_adapter_recipe
+}
+
+-- all items unlocked by tech
+local items = {
+    [1] = ufo_adapter_item
+}
+
+-- all entities unlocked by tech
+local entities = {
+    [1] = ufo_adapter_entity
+}
+
+-- create recipes and so on for each electric-pole
+for k, _ in pairs(data.raw["electric-pole"]) do
+    local adapted_name = 'ufo-adapted-' .. k
+    -- effect for tech
+    effects[#effects + 1] = { type = 'unlock-recipe', recipe = adapted_name }
+
+    -- make recipe
+    local recipe = data_util.copy_prototype(data.raw["recipe"][k], adapted_name)
+    recipe.enabled = false
+    recipe.ingredients = {
+        {type = 'item', name = k, amount = 1},
+        {type = 'item', name = 'ufo-adapter', amount = 1},
+    }
+    recipe.results = {{ type = 'item', name = adapted_name, amount = 1}}
+    recipe.localised_name = { "entity-name." .. k} -- TODO adapter/kompatibel in Text einbauen
+    --recipe.localised_description = { "entity-description." .. k} -- TODO hilft nicht - adapter/kompatibel in Text einbauen
+    recipes[#recipes + 1] = recipe
+
+    -- and item
+    local ufo_adapted_item = data_util.copy_prototype(data.raw["item"][k], adapted_name)
+    ufo_adapted_item.localised_name = { "entity-name." .. k} -- TODO adapter/kompatibel in Text einbauen
+    --ufo_adapted_item.localised_description = { "entity-description." .. k} -- hilft nicht -TODO adapter/kompatibel in Text einbauen
+    -- ufo_adapted_entity.tint = ....
+    items[#items + 1] = ufo_adapted_item
+
+    -- and entity
+    local ufo_adapted_entity = data_util.copy_prototype(data.raw["electric-pole"][k], adapted_name)
+    entities[#entities + 1] = ufo_adapted_entity
+end
+
+--- ufo technology
+local ufo_tech = {
+    name = 'ufo-tech',
+    type = 'technology',
+    icon = "__space-age__/graphics/icons/fulgoran-ruin-attractor.png",
+
+    prerequisites = { "space-platform" }, -- TODO
+    effects = effects,
+
+    research_trigger = { type = "scripted", trigger_description = {"description.ufo-tech", "5"}},
+    order = "c-e-b2",
+}
+
+
+local extensions = {}
+for _, item in pairs(items) do
+    extensions[#extensions + 1] = item
+end
+for _, entity in pairs(entities) do
+    extensions[#extensions + 1] = entity
+end
+for _, recipe in pairs(recipes) do
+    extensions[#extensions + 1] = recipe
+end
+
+extensions[#extensions + 1] = ufo_tech
+
+data:extend(extensions)
