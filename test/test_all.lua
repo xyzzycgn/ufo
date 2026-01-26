f7f74d5308a78c225f40746c0eb1264110a7273b
xyzzycgn <120264047+xyzzycgn@users.noreply.github.com>
2026-01-26 11:40:23 +0100

    tests

--- test/test_all.lua
+++ test/test_all.lua
@@ -11,6 +11,7 @@ BaseTest.hooked = true
 require('test.test_adapterHandling')
 require('test.test_ufo')
 require('test.test_scale')
+require('test.test_vaultHandling')
 
 BaseTest.hooked = false
 BaseTest:hookTests()
