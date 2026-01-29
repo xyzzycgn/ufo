f3275fcdb5ba899a614e1c3451bb50000f918d4b
xyzzycgn <120264047+xyzzycgn@users.noreply.github.com>
2026-01-29 16:49:13 +0100

    POC for a detector (for vault and shard) as equipment for vehicles (still incomplete) - images look ugly - not possible to mount detector in equipment of other vehicles

--- scripts/prototypes/car.lua
+++ scripts/prototypes/car.lua
@@ -38,7 +38,65 @@ for _, be in pairs(data.raw["solar-panel-equipment"]) do
     cat[#cat + 1] = "vehicle"
 end
 
--- TODO detector (tech?, recipe, equipment, item)
+
+
+
+-- TODO detector (tech, recipe, equipment, item)
+data:extend({
+    {
+        type = "item",
+        name = "detector-equipment",
+        icon = "__ufo__/graphics/icons/compass.png",
+        icon_size = 64,
+        scale = 0.5,
+        subgroup = "equipment",
+        order = "b[personal-transport]-c[detector]",
+        --inventory_move_sound = item_sounds.vehicle_inventory_move,
+        --pick_sound = item_sounds.vehicle_inventory_pickup,
+        --drop_sound = item_sounds.vehicle_inventory_move,
+        place_as_equipment_result = "detector-equipment",
+        stack_size = 10
+    },
+    {
+        type = "recipe",
+        name = "detector-equipment",
+        enabled = true, -- TODO tech
+        energy_required = 4,
+        ingredients = {
+            { type = "item", name = "iron-gear-wheel", amount = 2 },
+            { type = "item", name = "iron-plate", amount = 1 },
+        },
+        results = { { type = "item", name = "detector-equipment", amount = 1 } }
+    },
+})
+
+data:extend({
+    {
+        type = "battery-equipment",
+        name = "detector-equipment",
+        sprite = {
+            filename = "__ufo__/graphics/icons/compass.png",
+            width = 160,
+            height = 160,
+            scale = 0.5,
+        },
+        shape = {
+            width = 1,
+            height = 1,
+            type = "full",
+        },
+        energy_source = {
+            type = "electric",
+            buffer_capacity = "20kJ",
+            input_flow_limit = "10kW",
+            output_flow_limit = "10kW",
+            usage_priority = "primary-input"
+        },
+        categories = { "vehicle" }
+    },
+})
+
+Log.logBlock(data.raw["battery-equipment"], function(m)log(m)end, Log.CONFIG)
 
 -- TODO upgrade tech and new item/entity
 local car = data.raw.car.car
