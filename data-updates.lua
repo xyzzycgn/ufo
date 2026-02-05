a67fc158ca6ba6d325b6a42fe10c3fe2584c1261
xyzzycgn <120264047+xyzzycgn@users.noreply.github.com>
2026-02-05 09:58:56 +0100

    rename prototype files

--- data-updates.lua
+++ data-updates.lua
@@ -4,8 +4,8 @@
 local Log = require("__log4factorio__.Log")
 Log.setSeverity(Log.CONFIG)
 
-require("__ufo__.scripts.prototypes.fra")
-require("__ufo__.scripts.prototypes.fkh")
-require("__ufo__.scripts.prototypes.emp")
+require("__ufo__.scripts.prototypes.fulguran-ruin-attractor")
+require("__ufo__.scripts.prototypes.fulgoran-know-how")
+require("__ufo__.scripts.prototypes.electro-magnetic-plant")
 require("__ufo__.scripts.prototypes.recycler")
 
